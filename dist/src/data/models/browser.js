4a5038833e695aaec5ae4a423d8cdf2313485574
João Vítor Dos Santos Fontoura <34102271+Joao-b4@users.noreply.github.com>
2021-07-31 17:08:14 -0300

    Delete dist directory

--- dist/src/data/models/browser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ScrapingResult = void 0;
-class ScrapingResult {
-}
-exports.ScrapingResult = ScrapingResult;
-//# sourceMappingURL=browser.js.map
--- dist/src/main/webserver/factories/middlewares/search-by-stay-date--middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeSearcByStayDatehMiddleware = void 0;
-const middlewares_1 = require("@/presentation/http/middlewares");
-const makeSearcByStayDatehMiddleware = () => {
-    return new middlewares_1.SearchByStayDateMiddleware();
-};
-exports.makeSearcByStayDatehMiddleware = makeSearcByStayDatehMiddleware;
-//# sourceMappingURL=search-by-stay-date--middleware.js.map
